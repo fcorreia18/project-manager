14c39b20a087fd76c43ace5ceb308fbf0cf0aba4
FranciscoCorreia28 <francisco.correia1804@gmail.com>
2022-03-18 18:40:02 +0100

    fix: eslintrc.json

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -4,8 +4,6 @@ const app = express();
 
 app.use(express.json());
 
-app.get('/',(req, res)=>{
-    return res.json({message:"im here!"})
-});
+app.get("/", (req, res) => res.json({ message: "im here!" }));
 
 app.listen(3333);
