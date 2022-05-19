adb5f82e03463039f0b1a6520eb73ddfcd7d8456
FranciscoCorreia28 <francisco.correia1804@gmail.com>
2022-05-19 08:38:57 +0100

    feat: adding email validation when creating user

--- backend/src/services/user/CreateUserService.ts
+++ backend/src/services/user/CreateUserService.ts
@@ -1,5 +1,6 @@
 import { hash } from "bcryptjs";
 
+import AppError from "../../errors/AppError";
 import IUserRepository from "../../repositories/IUserRepository";
 import UserRepository from "../../repositories/UserRepository";
 
@@ -17,6 +18,10 @@ class CreateUserService {
 
     public async execute({ name, email, password }: IRequest) {
         const passwordHash = await hash(password, 8);
+        const findEmail = await this.userRepository.findByEmail(email);
+        if (findEmail) {
+            throw new AppError("Email já está em uso", 400);
+        }
         const user = await this.userRepository.create({
             name,
             email,
