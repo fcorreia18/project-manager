24b88678d04b58c067f7a3411fee273cca336dbb
FranciscoCorreia28 <francisco.correia1804@gmail.com>
2022-05-19 12:41:20 +0100

    feat: creating the ClientRepository and the method to findAll

---
+++ backend/src/repositories/client/ClientRepository.ts
@@ -0,0 +1,15 @@
+import { getRepository, Repository } from "typeorm";
+
+import { ICreateClientDTO } from "../../dtos/ICreateClientDTO";
+import Client from "../../models/Client";
+import IClientRepository from "./IClientRepository";
+
+export default class ClientRepository implements IClientRepository {
+    private ormRepository: Repository<Client>;
+    constructor() {
+        this.ormRepository = getRepository(Client);
+    }
+    async findAll(): Promise<Client[]> {
+        return this.ormRepository.find();
+    }
+}
