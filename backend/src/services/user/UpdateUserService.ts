465e6f19d491fd9a73953dff2b5385107c3e4a9e
FranciscoCorreia28 <francisco.correia1804@gmail.com>
2022-05-19 10:26:00 +0100

    feat: updateuserservice created

---
+++ backend/src/services/user/UpdateUserService.ts
@@ -0,0 +1,22 @@
+import ICreateUserDTO from "../../dtos/ICreateUserDTO";
+import AppError from "../../errors/AppError";
+import User from "../../models/User";
+import IUserRepository from "../../repositories/IUserRepository";
+import UserRepository from "../../repositories/UserRepository";
+
+export default class UpdateUserService {
+    private userRepository: IUserRepository;
+    constructor(userRepository: UserRepository) {
+        this.userRepository = userRepository;
+    }
+
+    async execute(id: number, user: ICreateUserDTO): Promise<User> {
+        const findUser = await this.userRepository.findById(id);
+        if (!findUser) {
+            throw new AppError("usuário não encontrado", 404);
+        }
+        const saveUser = Object.assign(findUser, user);
+        await this.userRepository.save(saveUser);
+        return saveUser;
+    }
+}
