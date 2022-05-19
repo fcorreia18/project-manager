24d7c893bd91218d40a05fa7b57319f01dcc0341
FranciscoCorreia28 <francisco.correia1804@gmail.com>
2022-05-19 11:58:45 +0100

    feat: created the migration method for clients

---
+++ backend/src/database/migrations/1652957128533-CreateClients.ts
@@ -0,0 +1,54 @@
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
+
+export class CreateClients1652957128533 implements MigrationInterface {
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(
+            new Table({
+                name: "clients",
+                columns: [
+                    {
+                        name: "id",
+                        type: "int",
+                        isPrimary: true,
+                        isGenerated: true,
+                        generationStrategy: "increment",
+                    },
+                    {
+                        name: "name",
+                        type: "varchar",
+                    },
+                    {
+                        name: "email",
+                        type: "varchar",
+                        isUnique: true,
+                    },
+                    {
+                        name: "phone",
+                        type: "varchar",
+                    },
+                    {
+                        name: "bi",
+                        type: "varchar",
+                        isUnique: true,
+                    },
+                    {
+                        name: "code",
+                        type: "varchar",
+                    },
+                    {
+                        name: "created_at",
+                        type: "timestamp",
+                        default: "now()",
+                    },
+                    {
+                        name: "updated_at",
+                        type: "timestamp",
+                        default: "now()",
+                    },
+                ],
+            })
+        );
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {}
+}
